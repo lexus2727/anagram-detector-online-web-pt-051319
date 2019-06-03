a8e2214cac20ba92bbcc2bcbb550b7847f3ef81f
Candice Parker <lexus2424@gmail.com>
2019-06-03 21:47:31 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,27 @@
 # Your code goes here!
+
+class Anagram
+  
+  attr_accessor :word
+  
+  def initialize(word)
+    @word = word
+end
+
+  def match(array_of_words)
+     array_of_words.select do |element|
+    @word.split("").sort == element.split("").sort
+    end
+  
+ end
+  
+end 
+  
+  
+ 
+ 
+ 
+  #[1,2,3,4,5].select {|num| num.even? }     #=> [2, 4]
+
+#a = %w[ a b c d e f ]
+#a.select {|v| v =~ /[aeiou]/ }    #=> ["a", "e"]
